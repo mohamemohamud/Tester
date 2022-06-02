c4ca0367e6beb29bac77cfb135967c5aff04ae1b
Mohamed Mohamud <mohamedmohamud@Mohameds-MacBook-Pro.local>
2022-06-01 21:31:07 -0400

    modified test run

---
+++ src/com/test/newtest.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class newtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.print("Hi there,");
+ System.out.print(" what time is it");
+	}
+
+}
